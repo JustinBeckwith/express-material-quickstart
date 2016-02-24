3a1b8019cd0ee61cb01253b9a0f84d1e1f8e5139
Justin Beckwith <justin.beckwith@gmail.com>
2016-02-23 22:41:07 -0800

    initial commit

---
+++ routes/index.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Node.js on Google Cloud' });
+});
+
+module.exports = router;
